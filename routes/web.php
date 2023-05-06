506906f211525f25d864f1cb0e1d094427ae354f
pindoyono <pindoyonopatan@gmail.com>
2023-05-07 06:20:18 +0800

    pes

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
+Route::view('/', 'welcome')->name('home');
+
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
         ->name('login');
